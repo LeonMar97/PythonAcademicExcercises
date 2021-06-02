6d50657232d54abe9389770cc72bef6bb785a388
ronehe <Ro.Heifetz@gmail.com>
2021-06-02 14:45:16 +0300

    changed to regex

--- Leon_Marekovich_Ronen_Heifetz_tar_5.py
+++ Leon_Marekovich_Ronen_Heifetz_tar_5.py
@@ -3,8 +3,21 @@ csvFileName ="us_postal_codes.csv"
 citiesFileName="2019_largest_cities.txt"
 cityFile=open(citiesFileName)
 csvFile=open(csvFileName)
-csvList=csvFile.read().split('\n')
+csvList=csvFile.read()
 
+biggestCities = cityFile.read()
+
+matchBiggestCities = re.findall(r"([A-Z][a-z].*[a-z])([A-Z].*?)\t", biggestCities)
+matchCsv = re.findall(r"(\d+),([A-Z][a-z]+),(.*?),", csvList)
+
+for tupCities in matchBiggestCities:
+    for tupCsv in matchCsv:
+        if tupCsv[1] + tupCsv[2] == tupCities[0] + tupCities[1]:
+            print(tupCsv[0] + ' ' + tupCsv[1] + ' ' + tupCsv[2])
+            break
+
+
+'''
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #splits by postal city and state
 def splitPostal(csvFile):
@@ -26,6 +39,7 @@ def bigCity():
 #decompose a string into "<city name> <state name> using REGEX
 def decompose(s):
     temp = re.search(r'[a-z][A-Z]', s)
+    #after finding where city ends and state begins, decompose and add ' ' in between
     return s[0:temp.span()[0]+1] + ' ' + s[temp.span()[0]+1:]
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,3 +48,4 @@ biggestCities = [decompose(x) for x in cities]
 allCities = splitPostal(csvList)
 PostalBigCities = [x[0] + ' ' + x[1] for x in allCities if x[1] in biggestCities]
 print(PostalBigCities)
+'''
